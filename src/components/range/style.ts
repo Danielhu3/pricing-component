9807f60ee22592d56d03a5220b959fdce82bec51
Danielhu3 <danielhu3123@gmail.com>
2022-11-15 18:10:23 -0300

    slider filling color

--- src/components/range/style.ts
+++ src/components/range/style.ts
@@ -1,4 +1,81 @@
 import styled from "styled-components";
-export const Range = styled.input`
+export const Range = styled.input.attrs((props: {value: number}) => props)`
     grid-area: range;
+    
+    appearance: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    //width: 100%;
+    height: 0.8rem;
+    //background: hsl(224, 65%, 95%);
+    border-radius: 0.8rem;
+    outline: none;
+    
+
+        background: ${(props) =>
+            `linear-gradient(to right,
+             hsl(174, 77%, 80%) 0%,
+             hsl(174, 77%, 80%) ${props.value *25}%,
+             hsl(224, 65%, 95%) ${props.value *25}%,
+             hsl(224, 65%, 95%) 100%);`};
+
+        
+
+    
+    &::-webkit-slider-thumb{
+        -webkit-appearance: none;
+        height: 3.5rem;
+        width: 3.5rem;
+        background-color: hsl(174, 86%, 45%);
+        box-shadow: 3px 10px 5px 5px hsla(174, 77%, 80%, 0.4);
+        border-radius: 50%;
+        cursor: pointer;
+        
+        &:active{
+            cursor: grabbing;
+            background-color: #0caa9b;
+        }
+    }
+
+   
+
+    &::-moz-range-thumb{
+        -moz-appearance: none;
+
+        height: 3.5rem;
+        width: 3.5rem;
+        background-color: hsl(174, 86%, 45%);
+        box-shadow: 3px 10px 5px 5px hsla(174, 77%, 80%, 0.4);
+        border-radius: 50%;
+        cursor: pointer;
+        
+        &:active{
+            cursor: grabbing;
+            background-color: #0caa9b;
+        }
+    }
+
+    &::-ms-thumb{
+        appearance: none;
+
+        height: 3.5rem;
+        width: 3.5rem;
+        background-color: hsl(174, 86%, 45%);
+        box-shadow: 3px 10px 5px 5px hsla(174, 77%, 80%, 0.4);
+        border-radius: 50%;
+        cursor: pointer;
+        
+        &:active{
+            cursor: grabbing;
+            background-color: #0caa9b;
+        }
+    }
+
+   
+
+   
+
+    
+  
+
 `
